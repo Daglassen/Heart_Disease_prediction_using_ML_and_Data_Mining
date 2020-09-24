302fde99e06281fdfacc6ca181f2abe949841311
Daglassen <daglarangelo@gmail.com>
2020-09-24 20:13:29 +0200

    for project 1

---
+++ project1.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt  
+import math                      
+import random
+import collections
+from collections import Counter  
+import pandas as pd         
