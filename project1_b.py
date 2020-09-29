268815d8187ab82f74d9d27f5b30a8c31d055861
Daglassen <daglarangelo@gmail.com>
2020-09-29 12:46:59 +0200

    project1

--- project1_b.py
+++ project1_b.py
@@ -1,7 +1,9 @@
-
+import pandas as pd
 import numpy as np
 import xlrd
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure, plot, title, xlabel, ylabel, show, legend
+from scipy.linalg import svd
 doc = xlrd.open_workbook('Data\SouthAfricanHeartDiseaseFixed.xlsx').sheet_by_index(0)
 #the history column has been moved into last position
 
@@ -34,17 +36,28 @@ attributeNames_c = attributeNames.copy()
 alldata=np.concatenate((X_c, np.expand_dims(y_c,axis=1)), axis=1)
 #complete matrix with all the data
 
+alldataframe=pd.DataFrame(alldata)
+alldataframe.describe()
+print(alldataframe.describe())
+#transformed alldata array into pandas dataframe
+#to calculate the basic statistics of the attributes
+
+
 
 chdlabels=doc.col_values(9,1,463)
 chdTypeflo = sorted(set(chdlabels))
 chdType=[int(i) for i in chdTypeflo]
 chdType[1]='sick'
 chdType[0]='healthy'
+#renamed all 0s & 1s to healthy and sick 
+#(it should be done with a dict though)
 
 y_r=alldata[:, 2]
 X_r=alldata[:,[0,1,3,4,5,6,7,8,9]]
+#created a matrix (y_r) with 1 attribute (chd) 
+#and a matrix (X_r) with the rest
 
-i = 1; j = 2;
+i = 3; j = 5;
 color = ['g','r']
 plt.title('Heart disease insidents')
 for c in range(len(chdType)):
@@ -58,5 +71,25 @@ plt.legend(chdType)
 plt.xlabel(attributeNames_c[i])
 plt.ylabel(attributeNames_c[j])
 plt.show()
+#scatter plot with coloured groups for sick and healthy
+#we only change i and j (numbers 0-8)
+
+
+Y = X - np.ones((N,1))*X.mean(axis=0)
+U,S,V = svd(Y,full_matrices=False)
+rho = (S*S) / (S*S).sum() 
+threshold = 0.9
+plt.figure()
+plt.plot(range(1,len(rho)+1),rho,'x-')
+plt.plot(range(1,len(rho)+1),np.cumsum(rho),'o-')
+plt.plot([1,len(rho)],[threshold, threshold],'k--')
+plt.title('Variance explained by principal components');
+plt.xlabel('Principal component');
+plt.ylabel('Variance explained');
+plt.legend(['Individual','Cumulative','Threshold'])
+plt.grid()
+plt.show()
+#PCA Maybe????
+
 
 
