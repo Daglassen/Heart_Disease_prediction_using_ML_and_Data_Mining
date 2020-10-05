bea1affcabe7c83a8db8756703075f992226f214
Daglassen <daglarangelo@gmail.com>
2020-10-05 11:27:01 +0200

    '1'

--- project1_b.py
+++ project1_b.py
@@ -4,7 +4,7 @@ import xlrd
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure, plot, title, xlabel, ylabel, show, legend
 from scipy.linalg import svd
-doc = xlrd.open_workbook('Data\SouthAfricanHeartDiseaseFixed.xlsx').sheet_by_index(0)
+doc = xlrd.open_workbook('Data\SouthAfricanHeartDisease.xlsx').sheet_by_index(0)
 #the history column has been moved into last position
 
 attributeNames = doc.row_values(rowx=0, start_colx=1, end_colx=11) 
@@ -16,7 +16,7 @@ HistoryDict = dict(zip(HistoryType,range(len(HistoryType))))
 #Transformed Family History into integers
 
 y = np.array([HistoryDict[value] for value in FamHistory])
-#the array with integers instead of strings
+#the family history column with integers instead of strings
 
 X = np.empty((462,9))
 for i in range(0,9):
@@ -52,30 +52,34 @@ chdType[0]='healthy'
 #renamed all 0s & 1s to healthy and sick 
 #(it should be done with a dict though)
 
-y_r=alldata[:, 2]
-X_r=alldata[:,[0,1,3,4,5,6,7,8,9]]
-#created a matrix (y_r) with 1 attribute (chd) 
-#and a matrix (X_r) with the rest
 
-i = 3; j = 5;
-color = ['g','r']
-plt.title('Heart disease insidents')
-for c in range(len(chdType)):
-    idx = y_c == c
-    plt.scatter(x=X_c[idx, i],
+#created a matrix (y_r) with 1 attribute (ldl) 
+#and a matrix (X_r) with the rest
+for i in range(8):
+    for j in range(8):
+    
+        color = ['g','r']
+        plt.title('Heart disease incidents')
+        for n in range(len(chdType)):
+            idx = y_c == n
+            plt.scatter(x=X_c[idx, i],
                 y=X_c[idx, j], 
-                c=color[c], 
+                c=color[n], 
                 s=20, alpha=0.5,
-                label=chdType[c])
-plt.legend(chdType)
-plt.xlabel(attributeNames_c[i])
-plt.ylabel(attributeNames_c[j])
-plt.show()
+                label=chdType[n])
+        plt.legend(chdType)
+        m, b = np.polyfit(X_c[idx, i], X_c[idx, j], 1)  #for linear regression
+        plt.plot(X_c[idx, i], m*X_c[idx, i] + b)
+
+        plt.xlabel(attributeNames_c[i])
+        plt.ylabel(attributeNames_c[j])
+        plt.show()
 #scatter plot with coloured groups for sick and healthy
-#we only change i and j (numbers 0-8)
+#we only change i and j (numbers 0-7)
 
 
 Y = X - np.ones((N,1))*X.mean(axis=0)
+Y = Y*(1/np.std(Y,0))
 U,S,V = svd(Y,full_matrices=False)
 rho = (S*S) / (S*S).sum() 
 threshold = 0.9
